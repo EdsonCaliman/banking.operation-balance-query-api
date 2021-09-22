686e6965ecd5b39ff14dfeb1a18ea226252a86df
Edson Caliman <edsonjunior@db1.com.br>
2021-09-22 11:18:10 -0300

    Solution, folders and initial projects

---
+++ Banking.Operation.Balance.Query.CrossCutting.Ioc/IoC.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Banking.Operation.Balance.Query.CrossCutting.Ioc.Modules;
+
+namespace Banking.Operation.Balance.Query.CrossCutting.Ioc
+{
+    public static class IoC
+    {
+        public static IServiceCollection ConfigureContainer(this IServiceCollection services, IConfiguration configuration)
+        {
+            DataModule.Register(services, configuration);
+            services.Register();
+            return services;
+        }
+    }
+}
---
+++ Banking.Operation.Balance.Query.CrossCutting.Ioc/Modules/DomainModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Banking.Operation.Balance.Query.CrossCutting.Ioc.Modules
+{
+    public static class DomainModule
+    {
+        public static void Register(this IServiceCollection services)
+        {
+
+        }
+    }
+}
